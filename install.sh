1d0531a8b98320b486ef6ad7e0c75e9870884ffd
Cameron Blocker <cameronjblocker@gmail.com>
2019-04-22 16:28:29 -0400

    updated scripts

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ source ./.bash_profile
 
 for file in '.bash_profile' '.bashrc' '.gitconfig'; do
     # Delete old symlink, or backup old file
-    test -h $HOME/$file && rm -f $HOME/$file || cp $HOME/$file $dotfileDir/backup$file
+    test -h $HOME/$file && rm -f $HOME/$file || mv $HOME/$file $dotfileDir/backup$file
     # link to our configs in the home directory
     ln -s $dotfileDir/$file $HOME/$file
 done 
 
 # ssh config has different naming pattern...
-test -h $HOME/.ssh/config && rm -f $HOME/.ssh/config || cp $HOME/.ssh/config $dotfileDir/backup.ssh-config
+test -h $HOME/.ssh/config && rm -f $HOME/.ssh/config || mv $HOME/.ssh/config $dotfileDir/backup.ssh-config
 ln -s $dotfileDir/.ssh-config $HOME/.ssh/config
--- teraform.sh
+++ teraform.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Setup my enviromnent via ssh on a new remote
 cat ~/.ssh/id_rsa.pub | ssh $1 'mkdir -p .ssh && cat >> .ssh/authorized_keys'
-ssh $1 'mkdir bin && git clone https://github.com/cjblocker/dotfiles.git && cd dotfiles && ./install.sh'
+ssh $1 'mkdir -p bin && mkdir -p Workspace && cd Workspace && git clone https://github.com/cjblocker/dotfiles.git && cd dotfiles && ./install.sh'
